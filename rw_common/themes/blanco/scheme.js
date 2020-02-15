f8c2fd33f5d9100087084f10d628c805ff508a58
Quang Nguyen <56195297+ntq1982@users.noreply.github.com>
2020-02-15 13:26:26 +0900

    Add files via upload

--- rw_common/themes/blanco/scheme.js
+++ rw_common/themes/blanco/scheme.js
@@ -1,35 +1,35 @@
 function activateDarkMode() {
-	const rootElement = document.querySelector(':root');
-	const darkTheme = {
-		'--background-color: #000000',
-		'--heading-color: #6699cc',
-		'--text-color: #999999',
-		'--link-color: #ffffff',
-		'--link-hover-color: #6699cc',
-		'--border-color: #cccccc',
-		'--other-color: #080808',
-		'--other-hover-color: #2b2b2b',
-	}
-	for(i in darkTheme) {
-		rootElement.style.setProperty(i, darkTheme[i]);
-	}
+    const rootElement = document.querySelector(':root')
+    const darkTheme = {
+        '--background-color': '#000000',
+        '--heading-color': '#6699cc',
+        '--text-color': '#999999',
+        '--link-color': '#ffffff',
+        '--link-hover-color': '#6699cc',
+        '--border-color': '#cccccc',
+        '--other-color': '#080808',
+        '--other-hover-color': '#2b2b2b'
+    }
+    for(k in darkTheme) {
+        rootElement.style.setProperty(k, darkTheme[k])
+    }
 }
 
 function activateLightMode() {
-	const rootElement = document.querySelector(':root');
-	const lightTheme = {
-		'--background-color: #ffffff',
-		'--heading-color: #6699cc',
-		'--text-color: #666666',
-		'--link-color: #000000',
-		'--link-hover-color: #6699cc',
-		'--border-color: #333333',
-		'--other-color: #f7f7f7',
-		'--other-hover-color: #d4d4d4',
-	}
-	for(i in lightTheme) {
-		rootElement.style.setProperty(i, lightTheme[i]);
-	}
+    const rootElement = document.querySelector(':root')
+    const lightTheme = {
+        '--background-color': '#ffffff',
+        '--heading-color': '#6699cc',
+        '--text-color': '#666666',
+        '--link-color': '#000000',
+        '--link-hover-color': '#6699cc',
+        '--border-color': '#333333',
+        '--other-color': '#f7f7f7',
+        '--other-hover-color': '#d4d4d4'
+    }
+    for(k in lightTheme) {
+        rootElement.style.setProperty(k, lightTheme[k])
+    }
 }
 
 /**
@@ -38,26 +38,26 @@ function activateLightMode() {
  * otherwise it will set a dark theme during night time
  **/
 function setColorScheme() {
-	const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-	const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches;
-	const isNotSpecified = window.matchMedia('(prefers-color-scheme: no-preference)').matches;
-	const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+    const isLightMode = window.matchMedia('(prefers-color-scheme: light)').matches
+    const isNotSpecified = window.matchMedia('(prefers-color-scheme: no-preference)').matches
+    const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified
 
-	window.matchMedia('(prefers-color-scheme: dark)').addListener(e => e.matches && activateDarkMode());
-	window.matchMedia('(prefers-color-scheme: light)').addListener(e => e.matches && activateLightMode());
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(e => e.matches && activateDarkMode())
+    window.matchMedia('(prefers-color-scheme: light)').addListener(e => e.matches && activateLightMode())
 
-	if(isDarkMode) activateDarkMode();
-	if(isLightMode) activateLightMode();
-	if(isNotSpecified || hasNoSupport) {
-		console.log('No preference for color scheme was specified or browser does not support it.');
-		console.log('Dark mode during night time is scheduled.');
-		now = new Date();
-		hour = now.getHours();
-		if (hour < 6 || hour >= 18) {
-			activateDarkMode();
-		}
-		else {
-			activateLightMode();
-		}
-	}
+    if(isDarkMode) activateDarkMode()
+    if(isLightMode) activateLightMode()
+    if(isNotSpecified || hasNoSupport) {
+        console.log('No preference for color scheme was specified or browser does not support it.')
+        console.log('Dark mode during night time is scheduled.')
+        now = new Date()
+        hour = now.getHours()
+        if (hour < 6 || hour >= 18) {
+            activateDarkMode()
+        }
+        else {
+            activateLightMode()
+        }
+    }
 }
